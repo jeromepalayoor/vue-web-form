616a6c786a579b8edb986c9dcb6c2e0739cbbfcc
jeromepalayoor <me@jp-ch.gq>
2023-03-29 23:08:01 +0800

    update

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -3,18 +3,35 @@
         <h1>Register</h1>
         <label>Email:</label>
         <input type="email" required v-model="email">
+
         <label>Password:</label>
         <input type="password" required v-model="password">
+
         <label>Role:</label>
         <select v-model="role">
             <option value="developer">Develop</option>
             <option value="designer">Design</option>
             <option value="tester">QA</option>
         </select>
+
+        <div class="terms">
+            <input type="checkbox" v-model="terms" required>
+            <label>Accept terms and Conditions</label>
+        </div>
+
+        <label>Skills:</label>
+        <input type="text" v-model="tempSkill" @keyup="addSkill">
+        <div v-for="skill in skills" :key="skill" class="pill">
+            <span @click="deleteSkill(skill)">{{ skill }}</span>
+        </div>
+
     </form>
+
     <p>Email: {{ email }}</p>
     <p>Password: {{ password }}</p>
     <p>Role: {{ role }}</p>
+    <p>Terms accepted: {{ terms }}</p>
+    <p>Skills: {{ skills }}</p>
 </template>
 
 <script>
@@ -23,7 +40,26 @@ export default {
         return {
             email: '',
             password: '',
-            role: 'tester'
+            role: 'tester',
+            terms: false,
+            tempSkill: '',
+            skills: []
+        }
+    },
+    methods: {
+        addSkill(e) {
+            if (e.key === ',' && this.tempSkill != ',' && this.tempSkill) {
+                this.tempSkill = this.tempSkill.slice(0, -1)
+                if (!this.skills.includes(this.tempSkill)) {
+                    this.skills.push(this.tempSkill)
+                }
+                this.tempSkill = ''
+            }
+        },
+        deleteSkill(skill) {
+            this.skills = this.skills.filter((item) => {
+                return skill !== item
+            })
         }
     }
 }
@@ -49,7 +85,8 @@ label {
     font-weight: bold;
 }
 
-input, select {
+input,
+select {
     display: block;
     padding: 10px 6px;
     width: 100%;
@@ -57,5 +94,26 @@ input, select {
     border: none;
     border-bottom: 1px solid #ddd;
     color: #555;
+}
+
+input[type="checkbox"] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+}
+
+.pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
 }
 </style>
