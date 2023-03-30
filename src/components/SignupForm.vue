106d4a51d43ede21667d091c3eadc310295eb6a4
jeromepalayoor <me@jp-ch.gq>
2023-03-30 14:59:57 +0800

    ok

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,11 +1,12 @@
 <template>
-    <form>
+    <form @submit.prevent="handleSubmit" v-if="showForm">
         <h1>Register</h1>
         <label>Email:</label>
         <input type="email" required v-model="email">
 
         <label>Password:</label>
         <input type="password" required v-model="password">
+        <div class="error" v-if="passwordError">{{ passwordError }}</div>
 
         <label>Role:</label>
         <select v-model="role">
@@ -14,24 +15,34 @@
             <option value="tester">QA</option>
         </select>
 
-        <div class="terms">
-            <input type="checkbox" v-model="terms" required>
-            <label>Accept terms and Conditions</label>
-        </div>
-
         <label>Skills:</label>
         <input type="text" v-model="tempSkill" @keyup="addSkill">
         <div v-for="skill in skills" :key="skill" class="pill">
             <span @click="deleteSkill(skill)">{{ skill }}</span>
         </div>
 
-    </form>
+        <div class="terms">
+            <input type="checkbox" v-model="terms" id="terms">
+            <label for="terms">Accept terms and Conditions</label>
+            <div class="error" v-if="termsError">{{ termsError }}</div>
+        </div>
 
-    <p>Email: {{ email }}</p>
-    <p>Password: {{ password }}</p>
-    <p>Role: {{ role }}</p>
-    <p>Terms accepted: {{ terms }}</p>
-    <p>Skills: {{ skills }}</p>
+        <div class="submit">
+            <button type="submit">Create an account</button>
+        </div>
+
+    </form>
+    <div class="formdone" v-if="!showForm">
+        <div><h1>Form Submitted!</h1></div>
+        <div class="data">Email: {{ email }}</div>
+        <div class="data">Password: {{ password }}</div>
+        <div class="data">Role: {{ role }}</div>
+        <div class="data">Skills:
+            <div v-for="skill in skills" :key="skill" class="pill done">
+                <span>{{ skill }}</span>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -43,7 +54,10 @@ export default {
             role: 'tester',
             terms: false,
             tempSkill: '',
-            skills: []
+            skills: [],
+            passwordError: '',
+            termsError: '',
+            showForm: true
         }
     },
     methods: {
@@ -60,19 +74,32 @@ export default {
             this.skills = this.skills.filter((item) => {
                 return skill !== item
             })
+        },
+        handleSubmit() {
+            //validate password
+            this.passwordError = this.password.length > 11 ? '' : '*Password must be at least 12 characters long'
+            this.termsError = this.terms ? '' : '*Terms and conditions must be accepted'
+            if (!this.passwordError && !this.termsError) {
+                this.showForm = false
+            }
         }
     }
 }
 </script>
 
 <style>
-form {
+form,
+.formdone {
     max-width: 420px;
     margin: 30px auto;
     background: white;
     text-align: left;
     padding: 40px;
     border-radius: 10px;
+}
+
+.formdone .data {
+    margin: 0 0 10px 0;
 }
 
 label {
@@ -94,6 +121,13 @@ select {
     border: none;
     border-bottom: 1px solid #ddd;
     color: #555;
+    transition: all 0.3s;
+    outline: none;
+}
+
+input:focus,
+select:focus {
+    border-bottom: 1px solid black;
 }
 
 input[type="checkbox"] {
@@ -106,7 +140,7 @@ input[type="checkbox"] {
 
 .pill {
     display: inline-block;
-    margin: 20px 10px 0 0;
+    margin: 0 5px 5px 5px;
     padding: 6px 12px;
     background: #eee;
     border-radius: 20px;
@@ -115,5 +149,29 @@ input[type="checkbox"] {
     font-weight: bold;
     color: #777;
     cursor: pointer;
+}
+
+.done.pill {
+    cursor: default;
+}
+
+.submit button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+}
+
+.submit {
+    text-align: center;
+}
+
+.error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
 }
 </style>
